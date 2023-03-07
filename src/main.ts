fd6736e493bfb86123f7e352a49d31de0919a3e2
BKrasnov <bkrasnov235@gmail.com>
2023-03-07 10:24:13 +0700

    Add cors ATS-1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app/app.module';
 import { PORT } from './environment';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   await app.listen(PORT, () => console.log('Server started on port ' + PORT));
 }
